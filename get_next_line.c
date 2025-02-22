6fd89ed6a5d8e183d480951b81632353a5aed192
Anas Elkadiri <aelkadir@e2r4p7.1337.ma>
2025-02-22 10:22:10 +0100

    final version

---
+++ get_next_line.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelkadir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 15:58:39 by aelkadir          #+#    #+#             */
+/*   Updated: 2024/11/24 16:21:05 by aelkadir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*ft_read(int fd, char *buff, char *stored)
+{
+	char	*stored_updated;
+	int		j;
+
+	while (1)
+	{
+		if (ft_strchr(stored, '\n'))
+			break ;
+		j = read(fd, buff, BUFFER_SIZE);
+		if (j < 0)
+		{
+			free(stored);
+			return (NULL);
+		}
+		if (j == 0)
+			return (stored);
+		buff[j] = '\0';
+		stored_updated = ft_strjoin(stored, buff);
+		free(stored);
+		if (!stored_updated)
+			return (NULL);
+		stored = stored_updated;
+	}
+	return (stored);
+}
+
+static char	*extract_line(char *stored)
+{
+	char	*line;
+	int		len;
+
+	if (!stored)
+		return (NULL);
+	len = 0;
+	while (stored[len] && stored[len] != '\n')
+		len++;
+	line = ft_substr(stored, 0, len + 1);
+	return (line);
+}
+
+static char	*update_stored(char *stored)
+{
+	char	*stored_updated;
+	int		len;
+
+	if (!stored)
+		return (NULL);
+	len = 0;
+	while (stored[len] && stored[len] != '\n')
+		len++;
+	if (stored[len] == '\n')
+		len++;
+	stored_updated = ft_strdup(&stored[len]);
+	free(stored);
+	if (stored_updated && stored_updated[0] == '\0')
+	{
+		free(stored_updated);
+		return (NULL);
+	}
+	return (stored_updated);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*stored;
+	char		*line;
+	char		*buff;
+
+	if (fd > OPEN_MAX || fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > INT_MAX)
+		return (NULL);
+	buff = malloc((size_t)BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
+	stored = ft_read(fd, buff, stored);
+	free(buff);
+	line = extract_line(stored);
+	stored = update_stored(stored);
+	return (line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aelkadir <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/24 15:58:56 by aelkadir          #+#    #+#             */
+/*   Updated: 2024/11/24 15:59:00 by aelkadir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <fcntl.h>
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*get_next_line(int fd);
+#endif
